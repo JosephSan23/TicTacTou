58ce37b4b13817f2d6c469cde36066ea875102ad
Joseph Olarte <josephsantiago2345@gmail.com>
2024-07-03 20:01:36 -0500

    Aplicando el tablero

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,47 @@
-import { useState } from 'react'
+import { Children, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const turns = {
+  X: 'x',
+  O: 'o'
+}
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+const Square = ({ children, updateBoard, index}) => {
+  return(
+    <div className='square'>
+      {children}
+    </div>
+  )
+}
+
+function App() {
+
+const [board, setBoard] = useState(['x', 'x', 'x', 'o', 'o', 'o', 'x', 'o', 'x'])
+// const board = Array(9).fill(null)
+
+  return(
+    <main className='board'>
+      <h1>Tic tac toe</h1>
+  
+      <section className='game'>
+        {
+          board.map((_, index) => {
+            return (
+              <Square
+                key={index}
+                index={index}
+              >
+                {board[index]}
+              </Square>
+                
+            )
+          })
+        }
+      </section>
+    </main>
   )
 }
 
